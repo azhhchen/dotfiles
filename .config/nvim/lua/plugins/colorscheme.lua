f5b430182e50c1bdb5654b6453f817ce68487768
Hung Hsun Chen <hunghsun@mail.com>
2025-03-27 23:12:43 +0800

    fix(nvim): disable colorscheme lazy loading

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,9 @@ return {
     "craftzdog/solarized-osaka.nvim",
     -- The event is for quick startuptime. If you find the theme blinking at startup unbearable
     -- or encounter any bugs, please consider commenting it out.
-    event = "UIEnter",
+    -- event = "UIEnter",
+    lazy = false,
+    priorty = 1000,
     config = function()
       require("solarized-osaka").setup({
         style = "night",
