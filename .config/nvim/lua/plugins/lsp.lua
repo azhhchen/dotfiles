dadc63349b8e499d7b9e31e06b43b917e8937f53
Hung Hsun Chen <204702481+azhhchen@users.noreply.github.com>
2025-04-08 15:23:15 +0800

    fix(nvim): mason didn't install the servers in opts

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -129,6 +129,15 @@ return { -- lspconfig
     },
     config = function(_, opts)
       require("mason").setup(opts)
+      local mr = require("mason-registry")
+      mr.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end)
     end
   }
 }
